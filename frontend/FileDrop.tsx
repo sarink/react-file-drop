a10cf98ae50e212e77023d353348bba9f0bb3b60
Kabir Sarin <sarink87@gmail.com>
2018-04-10 20:34:41 -0500

    Fix all event types, add proptypes

--- frontend/FileDrop.tsx
+++ frontend/FileDrop.tsx
@@ -1,22 +1,20 @@
 import PropTypes from 'prop-types';
-import React, { DragEvent } from 'react';
+import React, { DragEvent as ReactDragEvent, DragEventHandler as ReactDragEventHandler } from 'react';
 
 
 export type TDropEffects = 'copy' | 'move' | 'link' | 'none';
-export type TFrameDragEvent = DragEvent<IProps['frame']>
-export type TDivDragEvent = DragEvent<HTMLDivElement>;
 
 interface IProps {
   className?: string;
-  onDrop?: (files:FileList, event:TDivDragEvent) => any;
-  onDragOver?: Function;
-  onDragLeave?: Function;
+  frame?: HTMLElement | Document;
+  onFrameDragEnter?: (event:DragEvent) => void;
+  onFrameDragLeave?: (event:DragEvent) => void;
+  onFrameDrop?: (event:DragEvent) => void;
+  onDrop?: (files:FileList, event:ReactDragEvent<HTMLDivElement>) => any;
+  onDragOver?: ReactDragEventHandler<HTMLDivElement>;
+  onDragLeave?: ReactDragEventHandler<HTMLDivElement>;
   dropEffect?: TDropEffects;
   targetAlwaysVisible?: boolean;
-  frame?: HTMLElement | Document;
-  onFrameDragEnter?: Function;
-  onFrameDragLeave?: Function;
-  onFrameDrop?: Function;
 }
 interface IState {
   draggingOverFrame: boolean;
@@ -45,61 +43,37 @@ class FileDrop extends React.PureComponent<IProps, IState> {
     };
   }
 
-  // propTypes = {
-  //   onDrop: PropTypes.func,
-  //   onDragOver: PropTypes.func,
-  //   onDragLeave: PropTypes.func,
-  //   dropEffect: PropTypes.oneOf(["copy", "move", "link", "none"]),
-  //   targetAlwaysVisible: PropTypes.bool,
-  //   disableDoubleDrop: PropTypes.bool,
-  //   frame: function(props, propName, componentName) {
-  //     const prop = props[propName];
-  //     if (prop == null) {
-  //       return new Error("Warning: Required prop `" + propName + "` was not specified in `" + componentName + "`");
-  //     }
-  //     if (prop !== document && prop !== window && !(prop instanceof HTMLElement)) {
-  //       return new Error("Warning: Prop `" + propName + "` must be one of the following: document, window, or an HTMLElement!");
-  //     }
-  //   }
-  //   onFrameDragEnter: PropTypes.func,
-  //   onFrameDragLeave: PropTypes.func,
-  //   onFrameDrop: PropTypes.func
-  // };
+  static propTypes = {
+    onDrop: PropTypes.func,
+    onDragOver: PropTypes.func,
+    onDragLeave: PropTypes.func,
+    dropEffect: PropTypes.oneOf(["copy", "move", "link", "none"]),
+    targetAlwaysVisible: PropTypes.bool,
+    disableDoubleDrop: PropTypes.bool,
+    frame: function(props:any, propName:any, componentName:any) {
+      const prop = props[propName];
+      if (prop == null) {
+        return new Error("Warning: Required prop `" + propName + "` was not specified in `" + componentName + "`");
+      }
+      if (prop !== document && prop !== window && !(prop instanceof HTMLElement)) {
+        return new Error("Warning: Prop `" + propName + "` must be one of the following: document, HTMLElement!");
+      }
+    },
+    onFrameDragEnter: PropTypes.func,
+    onFrameDragLeave: PropTypes.func,
+    onFrameDrop: PropTypes.func
+  };
 
   resetDragging = () => {
     this.frameDragCounter = 0;
     this.setState({ draggingOverFrame: false, draggingOverTarget: false });
   }
 
-  handleDragStart = (event:TDivDragEvent) => {
+  handleWindowDragOverOrDrop = (event:DragEvent) => {
     event.preventDefault();
-    event.stopPropagation();
-    event.dataTransfer.effectAllowed = this.props.dropEffect;
   }
 
-  handleDragOver = (event:TDivDragEvent) => {
-    event.preventDefault();
-    event.stopPropagation();
-    this.setState({ draggingOverTarget: true });
-    if (!FileDrop.isIE()) event.dataTransfer.dropEffect = this.props.dropEffect;
-    if (this.props.onDragOver) this.props.onDragOver(event);
-  }
-
-  handleDragLeave = (event:TDivDragEvent) => {
-    this.setState({ draggingOverTarget: false });
-    if (this.props.onDragLeave) this.props.onDragLeave(event);
-  }
-
-  handleDrop = (event:TDivDragEvent) => {
-    event.preventDefault();
-    if (this.props.onDrop) {
-      const files = (event.dataTransfer) ? event.dataTransfer.files : null; // (event.frame) ? event.frame.files : undefined;
-      this.props.onDrop(files, event);
-      this.resetDragging();
-    }
-  }
-
-  handleFrameDrag = (event:Event) => {
+  handleFrameDrag = (event:DragEvent) => {
     // We are listening for events on the 'frame', so every time the user drags over any element in the frame's tree,
     // the event bubbles up to the frame. By keeping count of how many "dragenters" we get, we can tell if they are still
     // "draggingOverFrame" (b/c you get one "dragenter" initially, and one "dragenter"/one "dragleave" for every bubble)
@@ -107,7 +81,7 @@ class FileDrop extends React.PureComponent<IProps, IState> {
 
     if (this.frameDragCounter === 1) {
       this.setState({ draggingOverFrame: true });
-      if (this.props.onFrameDragLeave) this.props.onFrameDragEnter(event);
+      if (this.props.onFrameDragEnter) this.props.onFrameDragEnter(event);
       return;
     }
 
@@ -118,15 +92,39 @@ class FileDrop extends React.PureComponent<IProps, IState> {
     }
   }
 
-  handleFrameDrop = (event:Event) => {
+  handleFrameDrop = (event:DragEvent) => {
     if (!this.state.draggingOverTarget) {
       this.resetDragging();
       if (this.props.onFrameDrop) this.props.onFrameDrop(event);
     }
   }
 
-  handleWindowDragOverOrDrop = (event:Event) => {
+  handleDragStart:ReactDragEventHandler<HTMLDivElement> = (event) => {
     event.preventDefault();
+    event.stopPropagation();
+    event.dataTransfer.effectAllowed = this.props.dropEffect;
+  }
+
+  handleDragOver:ReactDragEventHandler<HTMLDivElement> = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setState({ draggingOverTarget: true });
+    if (!FileDrop.isIE()) event.dataTransfer.dropEffect = this.props.dropEffect;
+    if (this.props.onDragOver) this.props.onDragOver(event);
+  }
+
+  handleDragLeave:ReactDragEventHandler<HTMLDivElement> = (event) => {
+    this.setState({ draggingOverTarget: false });
+    if (this.props.onDragLeave) this.props.onDragLeave(event);
+  }
+
+  handleDrop:ReactDragEventHandler<HTMLDivElement> = (event) => {
+    event.preventDefault();
+    if (this.props.onDrop) {
+      const files = (event.dataTransfer) ? event.dataTransfer.files : null; // (event.frame) ? event.frame.files : undefined;
+      this.props.onDrop(files, event);
+      this.resetDragging();
+    }
   }
 
   stopFrameListeners = (frame:IProps['frame']) => {
