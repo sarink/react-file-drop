3ca33e1714f75fe3790027826880157ed10d1534
Kabir Sarin <sarink87@gmail.com>
2018-08-02 21:41:57 -0500

    Fix lint error in Demo

--- src/Demo/Demo.tsx
+++ src/Demo/Demo.tsx
@@ -31,7 +31,7 @@ class Demo extends React.Component {
   }
 
   render() {
-    var styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
+    const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
     return (
       <div id="react-file-drop-demo" style={styles}>
         <FileDrop onDrop={this.handleDrop}>
