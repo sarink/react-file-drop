beba1c605cc605b0d8e95f4c3ab23d60a897e560
Kabir Sarin <sarink87@gmail.com>
2020-04-01 00:20:45 -0500

    Update Demo to use functional component

--- demo/src/Demo.tsx
+++ demo/src/Demo.tsx
@@ -1,38 +1,22 @@
-import React, { DragEvent as ReactDragEvent } from 'react';
+import React from 'react';
 import { FileDrop } from 'react-file-drop';
 import './Demo.css';
 
-export class Demo extends React.Component {
-  // handleFrameDragEnter = (event:DragEvent) => {
-  //   console.log('handleFrameDragEnter', event);
-  // }
-  //
-  // handleFrameDragLeave = (event:DragEvent) => {
-  //   console.log('handleFrameDragLeave', event);
-  // }
-  //
-  // handleFrameDrop = (event:DragEvent) => {
-  //   console.log('handleFrameDrop', event);
-  // }
-  //
-  // handleDragOver:ReactDragEventHandler<HTMLDivElement> = (event) => {
-  //   console.log('handleDragOver', event);
-  // }
-  //
-  // handleDragLeave:ReactDragEventHandler<HTMLDivElement> = (event) => {
-  //   console.log('handleDragLeave', event);
-  // }
+export const Demo: React.FC = () => {
+  const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
 
-  handleDrop = (files: FileList | null, event: ReactDragEvent<HTMLDivElement>) => {
-    console.log('handleDrop!', files, event);
-  };
-
-  render() {
-    const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
-    return (
-      <div id="react-file-drop-demo" style={styles}>
-        <FileDrop onDrop={this.handleDrop}>Drop some files here!</FileDrop>
-      </div>
-    );
-  }
-}
+  return (
+    <div id="react-file-drop-demo" style={styles}>
+      <FileDrop
+        onFrameDragEnter={(event) => console.log('onFrameDragEnter', event)}
+        onFrameDragLeave={(event) => console.log('onFrameDragLeave', event)}
+        onFrameDrop={(event) => console.log('onFrameDrop', event)}
+        onDragOver={(event) => console.log('onDragOver', event)}
+        onDragLeave={(event) => console.log('onDragLeave', event)}
+        onDrop={(files, event) => console.log('onDrop!', files, event)}
+      >
+        Drop some files here!
+      </FileDrop>
+    </div>
+  );
+};
