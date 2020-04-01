311baa44e5d9854253c13b2b6c535c0b61e3eb92
Kabir Sarin <sarink87@gmail.com>
2020-04-01 09:39:17 -0500

    Use raw-loader to display demoText in index.html

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import demoText from '!!raw-loader!./Demo.tsx';
 import { Demo } from './Demo';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.getElementById('demo'));
+ReactDOM.render(<div>{demoText}</div>, document.getElementById('demo-text'));
