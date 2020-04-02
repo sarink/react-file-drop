523be293e2b7b07e2e5ad6323bb06452d4fe758d
Kabir Sarin <sarink87@gmail.com>
2020-04-02 18:08:57 -0500

    Update demo so it's clear the code is running

--- demo/src/Demo.tsx
+++ demo/src/Demo.tsx
@@ -4,20 +4,23 @@ import React from 'react';
 import { FileDrop } from 'react-file-drop';
 import './Demo.css';
 
-export const Demo: React.FC = () => {
+export const Demo = () => {
   const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
   return (
-    <div style={styles}>
-      <FileDrop
-        onFrameDragEnter={(event) => console.log('onFrameDragEnter', event)}
-        onFrameDragLeave={(event) => console.log('onFrameDragLeave', event)}
-        onFrameDrop={(event) => console.log('onFrameDrop', event)}
-        onDragOver={(event) => console.log('onDragOver', event)}
-        onDragLeave={(event) => console.log('onDragLeave', event)}
-        onDrop={(files, event) => console.log('onDrop!', files, event)}
-      >
-        Drop some files here!
-      </FileDrop>
+    <div>
+      <h1>React File Drop demo</h1>
+      <div style={styles}>
+        <FileDrop
+          onFrameDragEnter={(event) => console.log('onFrameDragEnter', event)}
+          onFrameDragLeave={(event) => console.log('onFrameDragLeave', event)}
+          onFrameDrop={(event) => console.log('onFrameDrop', event)}
+          onDragOver={(event) => console.log('onDragOver', event)}
+          onDragLeave={(event) => console.log('onDragLeave', event)}
+          onDrop={(files, event) => console.log('onDrop!', files, event)}
+        >
+          Drop some files here!
+        </FileDrop>
+      </div>
     </div>
   );
 };
